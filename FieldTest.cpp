70e8387a7ef8ac9c17c30fcccd390ee7010c60ec
mwoolsey <mwoolsey625@gmail.com>
2016-09-26 11:45:41 -0700

    added a few tests and added found bugs to buglog.md

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,20 +5,34 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
-class FieldTest : public ::testing::Test
-{
-	protected:
-		FieldTest(){}
-		virtual ~FieldTest(){}
-		virtual void SetUp(){}
-		virtual void TearDown(){}
+
+class FieldTest : public ::testing::Test {
+ protected:
+  FieldTest() {}
+  virtual ~FieldTest() {}
+  virtual void SetUp() {}
+  virtual void TearDown() {}
 };
 
-TEST(FieldTest, placeMineInBounds)
-{
-	Field minefield;
-	
-	minefield.placeMine(4,5);
-	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+TEST(FieldTest, placeMineInBounds) {
+  Field minefield;
+
+  minefield.placeMine(4, 5);
+  ASSERT_EQ(MINE_HIDDEN, minefield.get(4, 5));
+}
+
+TEST(Field, placeMineOutOfBoundsUnder) {
+  Field minefield;
+  minefield.placeMine(-1, 0);
+  ASSERT_ANY_THROW(minefield.get(-1, 0));
+}
+
+TEST(Field, revealAdjacentOutOfBoundsUnder) {
+  Field minefield;
+  ASSERT_ANY_THROW(minefield.revealAdjacent(-1, 1));
+}
+
+TEST(FieldTest, placeMineOutOfBoundsOver) {
+  Field minefield;
+  ASSERT_ANY_THROW(minefield.placeMine(10, 10));
 }
--- buglog.md
+++ buglog.md
@@ -1,1 +1,1 @@
-# BUG LOG
+# BUG LOG  The FIELD_DIMENSION is set to 10 by default but in the function inBounds the test goes out of bounds when running on 10,10 since the vector is zero based.  In the revealAdjacent function the logic test should have an && after the bounds check instead of an or. 
